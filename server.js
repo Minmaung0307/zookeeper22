26880bf905012359357f4ff75cf742bd4acc88dc
Min Maung <minmaung0307@gmail.com>
2022-09-10 17:57:07 -0400

    add heroku

--- server.js
+++ server.js
@@ -49,14 +49,34 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
-  //   console.log(req.query);
   res.json(results);
 });
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+//   let results = animals;
+//   if (req.query) {
+//     results = filterByQuery(req.query, results);
+//   }
+//   console.log(req.query);
+//   res.json(result);
+// });
 
 // app.get("/api/animals", (req, res) => {
 //   res.send("မင်္ဂလာပါ နေကောင်းပါစ ကျန်းမာပါစေ!");
